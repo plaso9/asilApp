04c1615bcdbb69d7fa5ad0a9ced41506a9e8731d
plaso9 <piepoligiulio7@gmail.com>
2019-07-21 15:54:03 +0200

    Homepage asilApp wih gridLayout and Cards

--- app/src/main/java/it/uniba/di/sms/asilapp/MainActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/MainActivity.java
@@ -2,12 +2,15 @@ package it.uniba.di.sms.asilapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+    GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridLayout=findViewById(R.id.gridHomeLayout);
     }
 }
