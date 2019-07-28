d3342fec0647d6fe3415ecb41d556b39db853980
Davide Santacesaria <d.santacesaria2@studenti.uniba.it>
2019-07-28 18:55:30 +0200

    Added new HomepageActivity, first page after login

---
+++ app/src/main/java/it/uniba/di/sms/asilapp/HomepageActivity.java
@@ -0,0 +1,61 @@
+package it.uniba.di.sms.asilapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
+
+public class HomepageActivity extends AppCompatActivity {
+    GridLayout gridLayout;
+    CardView card_view_PersonalData;
+    CardView card_view_Informative;
+    CardView card_view_Questionnaires;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        //defined gridlayout variable
+        gridLayout=findViewById(R.id.gridHomeLayout);
+        //defined card variable
+        card_view_PersonalData = findViewById(R.id.card_personalData);
+        card_view_Informative = findViewById(R.id.card_informationSection);
+        card_view_Questionnaires = findViewById(R.id.card_questionnaires);
+
+
+        //set function to card
+        card_view_Informative.setOnClickListener(card_view_Informative_listener);
+        card_view_PersonalData.setOnClickListener(card_view_Personaldata_listener);
+        card_view_Questionnaires.setOnClickListener(card_view_Questionnaries_listener);
+
+
+    }
+
+    public View.OnClickListener card_view_Informative_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent sens = new Intent (HomepageActivity.this,InformativeActivity.class);
+            startActivity(sens);
+
+        }
+    };
+
+    public View.OnClickListener card_view_Personaldata_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent sens = new Intent (HomepageActivity.this,PersonalDataActivity.class);
+            startActivity(sens);
+
+        }
+    };
+
+    public View.OnClickListener card_view_Questionnaries_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), QuestionnairesActivity.class);
+            startActivity(i);
+        }
+    };
+}
