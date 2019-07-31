455f47503b23f433765592edb42ae37a3aee0186
f.pollicelli <f.pollicelli@studenti.uniba.it>
2019-07-31 12:59:00 +0200

    Created searchPatientActivity

--- app/src/main/java/it/uniba/di/sms/asilapp/DoctorActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/DoctorActivity.java
@@ -1,13 +1,61 @@
 package it.uniba.di.sms.asilapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
 
 public class DoctorActivity extends AppCompatActivity {
+    GridLayout gridLayout;
+    CardView  card_view_searchPatient,
+            card_view_kitOpening,
+            card_view_patientList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doctor);
+        setContentView(R.layout.activity_myinfo);
+
+        //defined gridlayout variable
+        gridLayout=findViewById(R.id.gridDoctorLayout);
+
+        //defined card variable
+        card_view_searchPatient = findViewById(R.id.card_searchPatient);
+        card_view_kitOpening = findViewById(R.id.card_kitOpening);
+        card_view_patientList = findViewById(R.id.card_patientList);
+
+        card_view_searchPatient.setOnClickListener(card_view_searchPatient_listener);
+        card_view_kitOpening.setOnClickListener(card_view_kitOpening_listener);
+        card_view_patientList.setOnClickListener(card_view_patientList_listener);
+
     }
+
+    public View.OnClickListener card_view_searchPatient_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent sens = new Intent (DoctorActivity.this,SearchPatientActivity.class);
+            startActivity(sens);
+
+        }
+    };
+    public View.OnClickListener card_view_kitOpening_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent sens = new Intent (DoctorActivity.this,MyInfoActivity.class);
+            startActivity(sens);
+
+        }
+    };
+
+    public View.OnClickListener card_view_patientList_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent sens = new Intent (DoctorActivity.this,MyInfoActivity.class);
+            startActivity(sens);
+
+        }
+    };
+
 }
