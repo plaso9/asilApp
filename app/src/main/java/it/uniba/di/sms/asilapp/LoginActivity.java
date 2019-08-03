8c0a8d519837fbb32d63a7e05153b28c6a2881e7
plaso9 <piepoligiulio7@gmail.com>
2019-08-03 11:07:13 +0200

    Set message if login Failed

--- app/src/main/java/it/uniba/di/sms/asilapp/LoginActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -46,11 +47,16 @@ public class LoginActivity extends AppCompatActivity {
                                     startActivity(intent);
                                     finish();
                                 } else {
+                                    onLoginFailed();
                                     return;
                                 }
                             }
                         });
             }
         });
+    }
+
+    public void onLoginFailed(){
+        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
     }
 }
