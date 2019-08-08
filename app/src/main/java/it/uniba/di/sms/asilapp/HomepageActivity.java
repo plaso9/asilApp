f139cee6c89cf51b48d2b572278d43863b2c454c
dpalmisano26 <palmisano95lp@gmail.com>
2019-08-08 12:45:02 +0200

    Second implementation of Burger Menu

--- app/src/main/java/it/uniba/di/sms/asilapp/HomepageActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/HomepageActivity.java
@@ -1,23 +1,52 @@
 package it.uniba.di.sms.asilapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     GridLayout gridLayout;
     CardView card_view_PersonalData;
     CardView card_view_Informative;
     CardView card_view_Questionnaires;
     CardView card_view_MedicalRecords;
 
+
+    private DrawerLayout drawer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
         //defined gridlayout variable
         gridLayout=findViewById(R.id.gridHomeLayout);
         //defined card variable
@@ -36,6 +65,45 @@ public class HomepageActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent sens;
+        switch (item.getItemId()){
+            case R.id.nav_info:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (HomepageActivity.this, InformativeActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_medicalRecords:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (HomepageActivity.this, MedicalRecordsActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_personalData:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (HomepageActivity.this, PersonalDataActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_questionnaires:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (HomepageActivity.this, QuestionnairesActivity.class);
+                startActivity(sens);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
 
     public View.OnClickListener card_view_Informative_listener = new View.OnClickListener() {
         @Override
