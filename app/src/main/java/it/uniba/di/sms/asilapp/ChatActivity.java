7dd259e55faa59eed1dead30ec835ce8e09e3032
dpalmisano26 <palmisano95lp@gmail.com>
2019-08-20 12:19:04 +0200

    Added menu to Land Layouts

--- app/src/main/java/it/uniba/di/sms/asilapp/ChatActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/ChatActivity.java
@@ -1,13 +1,20 @@
 package it.uniba.di.sms.asilapp;
 
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -32,7 +39,7 @@ import java.util.List;
 import it.uniba.di.sms.asilapp.adapter.MessageAdapter;
 import it.uniba.di.sms.asilapp.models.Message;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private static final String TAG = "ChatActivity";
     
     private EditText mMessage;
@@ -43,10 +50,27 @@ public class ChatActivity extends AppCompatActivity {
     List<Message> mMessagesList;
     RecyclerView recyclerView;
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         mMessage = findViewById(R.id.message);
         mSend = findViewById(R.id.sendMessage);
@@ -62,6 +86,52 @@ public class ChatActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(linearLayoutManager);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent sens;
+        switch (item.getItemId()){
+            case R.id.nav_info:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (ChatActivity.this, InformativeActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_medicalRecords:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (ChatActivity.this, MedicalRecordsActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_personalData:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (ChatActivity.this, PersonalDataActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_questionnaires:
+                drawer.closeDrawer(GravityCompat.START);
+                sens = new Intent (ChatActivity.this, QuestionnairesActivity.class);
+                startActivity(sens);
+                break;
+            case R.id.nav_logout:
+                drawer.closeDrawer(GravityCompat.START);
+                FirebaseAuth.getInstance().signOut();
+                sens = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(sens);
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
 
     @Override
     protected void onStart(){
