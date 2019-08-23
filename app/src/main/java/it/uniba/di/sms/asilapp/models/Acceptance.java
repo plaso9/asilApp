a23c0203d2f75d34a2b00cee475c2d14f40b96e2
Davide Santacesaria <d.santacesaria2@studenti.uniba.it>
2019-08-23 16:04:46 +0200

    Removed unused methods and parameter

--- app/src/main/java/it/uniba/di/sms/asilapp/models/Acceptance.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/models/Acceptance.java
@@ -6,20 +6,12 @@ public class Acceptance {
     public String id;
     public String name;
     public String address;
-    public String regulation;
     public ArrayList<String> listOfServices;
     public long city;
 
     //empty constructor
     public Acceptance(){
 
-    }
-
-    //constructor
-    public Acceptance(String name, String address, String regulation){
-        this.name = name;
-        this.address = address;
-        this.regulation = regulation;
     }
     //constructor
     public Acceptance(String name, String address, ArrayList<String> listOfServices, String id, long city){
@@ -44,14 +36,6 @@ public class Acceptance {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public String getRegulation() {
-        return regulation;
-    }
-
-    public void setRegulation(String regulation) {
-        this.regulation = regulation;
     }
 
     public String getId() {
