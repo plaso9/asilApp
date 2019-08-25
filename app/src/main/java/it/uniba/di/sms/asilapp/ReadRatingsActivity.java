eeb5784716c8aae587c7f5318c5365ea63fad6ef
plaso9 <piepoligiulio7@gmail.com>
2019-08-25 14:33:27 +0200

    Code Refactoring

--- app/src/main/java/it/uniba/di/sms/asilapp/ReadRatingsActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/ReadRatingsActivity.java
@@ -24,22 +24,25 @@ import it.uniba.di.sms.asilapp.models.Rating;
 import it.uniba.di.sms.asilapp.models.User;
 
 public class ReadRatingsActivity extends AppCompatActivity {
+    //variable declaration
     private static final String TAG = "ReadRatingsActivity";
-
-    private RecyclerView recyclerView;
     private DatabaseReference mDatabaseRating;
+    private RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {    //Called when the activity is starting.
         super.onCreate(savedInstanceState);
+        //Set the activity content from a layout resource.
         setContentView(R.layout.activity_read_ratings);
 
+        //Defined variable
+        recyclerView = findViewById(R.id.ratingList);
+
         //Initialize Database Reference
-        mDatabaseRating = FirebaseDatabase.getInstance().getReference().child("rating");
+        mDatabaseRating = FirebaseDatabase.getInstance().getReference("rating");
         //Used to synchronize data
         mDatabaseRating.keepSynced(true);
 
-        recyclerView = findViewById(R.id.ratingList);
         //Used to know size top avoid expensive layout operation
         recyclerView.setHasFixedSize(true);
         //Create layout manager, it is responsible for measuring and positioning item views within a RecyclerView
@@ -47,25 +50,33 @@ public class ReadRatingsActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart(){   //Called when the activity had been stopped,
         super.onStart();
-        FirebaseRecyclerAdapter<Rating, RatinViewHolder> firebaseRecyclerAdapter = new
-                FirebaseRecyclerAdapter<Rating, RatinViewHolder>(
-                        Rating.class, R.layout.activity_rating_row, RatinViewHolder.class, mDatabaseRating
+        //Let's create new ViewHolder
+        FirebaseRecyclerAdapter<Rating, RatingViewHolder> firebaseRecyclerAdapter = new
+                FirebaseRecyclerAdapter<Rating, RatingViewHolder>(
+                        Rating.class, R.layout.activity_rating_row, RatingViewHolder.class, mDatabaseRating
                 ) {
                     @Override
-                    protected void populateViewHolder(final RatinViewHolder viewHolder, final Rating model, final int position) {
+                    //It's used to populate new ViewHolder
+                    protected void populateViewHolder(final RatingViewHolder viewHolder, final Rating model, final int position) {
+                        //Initialize Database Reference
                         DatabaseReference db = FirebaseDatabase.getInstance().getReference("user").child(model.getUser());
+                        //Retrieve data
                         db.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
+                            //called with a snapshot of the data at this location
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                //Get Value
                                 User user = dataSnapshot.getValue(User.class);
+                                //Set Value
                                 viewHolder.setName(user.getName()+" "+user.getSurname());
                                 viewHolder.setAvgRating(roundTwoDecimals(model.getAvgRating()));
                                 viewHolder.setComment(model.getComment());
                             }
 
                             @Override
+                            //triggered in the event that this listener either failed at the server, or is removed as a result of the security and Firebase rules.
                             public void onCancelled(@NonNull DatabaseError databaseError) {
                                 // Getting user failed
                                 Log.w(TAG, "loadUser:onCancelled", databaseError.toException());
@@ -74,7 +85,7 @@ public class ReadRatingsActivity extends AppCompatActivity {
                             }
                         });
 
-
+                        //Set a click listener on the item of ViewHolder objects
                         viewHolder.itemView.setOnClickListener(new View.OnClickListener(){
                             @Override
                             public void onClick(View view){
@@ -82,24 +93,27 @@ public class ReadRatingsActivity extends AppCompatActivity {
                                 String user_clicked = getRef(position).getKey();
                                 //Create new Intent
                                 Intent patientDetailIntent = new Intent(ReadRatingsActivity.this, PatientDetailActivity.class);
+                                //Pass data between intents
                                 patientDetailIntent.putExtra("user_clicked", user_clicked);
                                 startActivity(patientDetailIntent);
                             }
                         });
                     }
                 };
-
+        //Called to associate adapter with the list
         recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
-    public static class RatinViewHolder extends RecyclerView.ViewHolder{
+    //Created ViewHolder Class
+    public static class RatingViewHolder extends RecyclerView.ViewHolder{
         View mView;
 
-        public RatinViewHolder(View itemView){
+        public RatingViewHolder(View itemView){
             super(itemView);
             mView = itemView;
         }
 
+        //Set functions
         public void setName(String name){
             TextView userName = mView.findViewById(R.id.text_userNameClicked);
             userName.setText(name);
@@ -118,10 +132,9 @@ public class ReadRatingsActivity extends AppCompatActivity {
 
     }
 
-    public float roundTwoDecimals(float d)
-    {
+    //Function to round avg rating
+    public float roundTwoDecimals(float d) {
         DecimalFormat twoDForm = new DecimalFormat("#.##");
-
         return Float.valueOf(twoDForm.format(d));
     }
 }
