f85f15ba07ff868b30b41f1c7ebdb5f417eb7cef
plaso9 <piepoligiulio7@gmail.com>
2019-08-26 15:47:43 +0200

    Fix bug menu for user role

--- app/src/main/java/it/uniba/di/sms/asilapp/PathologyActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/PathologyActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -18,6 +19,7 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,10 +27,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import it.uniba.di.sms.asilapp.models.Pathology;
+import it.uniba.di.sms.asilapp.models.User;
 
 public class PathologyActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private static final String TAG = "PathologyActivity";
-
+    private DatabaseReference mUserReference;
     private String pathology_id;
     private EditText mName;
     private EditText mDescription;
@@ -39,7 +42,19 @@ public class PathologyActivity extends AppCompatActivity implements NavigationVi
 
     private ImageButton imgBtnLanguage;
     private DrawerLayout drawer;
-
+    private MenuItem nav_home;
+    private MenuItem nav_info;
+    private MenuItem nav_addUser;
+    private MenuItem nav_homeDoctor;
+    private MenuItem nav_kitOpening;
+    private MenuItem nav_readRatings;
+    private MenuItem nav_personalData;
+    private MenuItem nav_addAcceptance;
+    private MenuItem nav_searchPatient;
+    private MenuItem nav_medicalRecords;
+    private MenuItem nav_questionnaires;
+    private MenuItem nav_visitedPatient;
+    private MenuItem nav_addRetrieveNecessities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +74,8 @@ public class PathologyActivity extends AppCompatActivity implements NavigationVi
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        //Get user role to hide some menu item
+        getUserRole();
 
 
 
@@ -101,34 +118,26 @@ public class PathologyActivity extends AppCompatActivity implements NavigationVi
         mPathologyReference.addListenerForSingleValueEvent(pathologyListener);
         imgBtnLanguage = findViewById(R.id.imgBtnLanguage);
         imgBtnLanguage.setOnClickListener(imgBtnLanguage_listener);
-
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (requestCode == 1) { // english
-
             if (resultCode == Activity.RESULT_CANCELED) {
                 imgBtnLanguage.setImageResource(R.drawable.lang);
                 Intent refresh = new Intent(this, PathologyActivity.class);
                 startActivity(refresh);
                 this.finish();
             }
-
-
         }
         if (requestCode == 2) { //italian
-
             if (resultCode == Activity.RESULT_CANCELED) {
                 imgBtnLanguage.setImageResource(R.drawable.italy);
                 Intent refresh = new Intent(this, PathologyActivity.class);
                 startActivity(refresh);
                 this.finish();
             }
-
         }
     }
 
@@ -143,33 +152,68 @@ public class PathologyActivity extends AppCompatActivity implements NavigationVi
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Intent sens;
         switch (item.getItemId()){
+            case R.id.nav_homeDoctor:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_homeDoctorIntent = new Intent (PathologyActivity.this, DoctorActivity.class);
+                startActivity(nav_homeDoctorIntent);
+                break;
+            case R.id.nav_home:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_homeIntent = new Intent (PathologyActivity.this, HomepageActivity.class);
+                startActivity(nav_homeIntent);
+                break;
             case R.id.nav_info:
                 drawer.closeDrawer(GravityCompat.START);
-                sens = new Intent (PathologyActivity.this, InformativeActivity.class);
-                startActivity(sens);
+                //Create new Intent
+                Intent nav_infoIntent = new Intent (PathologyActivity.this, InformativeActivity.class);
+                startActivity(nav_infoIntent);
                 break;
             case R.id.nav_medicalRecords:
                 drawer.closeDrawer(GravityCompat.START);
-                sens = new Intent (PathologyActivity.this, MedicalRecordsActivity.class);
-                startActivity(sens);
+                //Create new Intent
+                Intent nav_medicalRecordsIntent = new Intent (PathologyActivity.this, MedicalRecordsActivity.class);
+                startActivity(nav_medicalRecordsIntent);
                 break;
             case R.id.nav_personalData:
                 drawer.closeDrawer(GravityCompat.START);
-                sens = new Intent (PathologyActivity.this, PersonalDataActivity.class);
-                startActivity(sens);
+                //Create new Intent
+                Intent nav_personalDataIntent  = new Intent (PathologyActivity.this, PersonalDataActivity.class);
+                startActivity(nav_personalDataIntent);
                 break;
             case R.id.nav_questionnaires:
                 drawer.closeDrawer(GravityCompat.START);
-                sens = new Intent (PathologyActivity.this, QuestionnairesActivity.class);
-                startActivity(sens);
+                //Create new Intent
+                Intent nav_questionnairesIntent = new Intent (PathologyActivity.this, QuestionnairesActivity.class);
+                startActivity(nav_questionnairesIntent);
+                break;
+            case R.id.nav_search_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_searchPatientIntent = new Intent (PathologyActivity.this, SearchPatientActivity.class);
+                startActivity(nav_searchPatientIntent);
+                break;
+            case R.id.nav_kit_opening:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_kitOpeningIntent = new Intent (PathologyActivity.this, KitOpeningActivity.class);
+                startActivity(nav_kitOpeningIntent);
+                break;
+            case R.id.nav_visited_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_visitedPatientIntent = new Intent (PathologyActivity.this, PatientListActivity.class);
+                startActivity(nav_visitedPatientIntent);
                 break;
             case R.id.nav_logout:
                 drawer.closeDrawer(GravityCompat.START);
+                //Sign out function
                 FirebaseAuth.getInstance().signOut();
-                sens = new Intent(PathologyActivity.this, MainActivity.class);
-                startActivity(sens);
+                //Create new Intent
+                Intent nav_logoutIntent = new Intent(PathologyActivity.this, MainActivity.class);
+                startActivity(nav_logoutIntent);
                 finish();
                 break;
         }
@@ -183,5 +227,86 @@ public class PathologyActivity extends AppCompatActivity implements NavigationVi
         }else{
             super.onBackPressed();
         }
+    }
+
+    public void removeItemDoctor(){
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // get menu from navigationView
+        Menu menu = navigationView.getMenu();
+        // find MenuItem you want to change
+        nav_homeDoctor = menu.findItem(R.id.nav_homeDoctor);
+        nav_kitOpening = menu.findItem(R.id.nav_kit_opening);
+        nav_searchPatient = menu.findItem(R.id.nav_search_patient);
+        nav_visitedPatient = menu.findItem(R.id.nav_visited_patient);
+        //Set item visibility
+        nav_homeDoctor.setVisible(false);
+        nav_kitOpening.setVisible(false);
+        nav_searchPatient.setVisible(false);
+        nav_visitedPatient.setVisible(false);
+    }
+
+    public void removeItemUser(){
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // get menu from navigationView
+        Menu menu = navigationView.getMenu();
+        // find MenuItem you want to change
+        nav_home = menu.findItem(R.id.nav_home);
+        nav_info = menu.findItem(R.id.nav_info);
+        nav_personalData = menu.findItem(R.id.nav_personalData);
+        nav_medicalRecords = menu.findItem(R.id.nav_medicalRecords);
+        nav_questionnaires = menu.findItem(R.id.nav_questionnaires);
+        //Set item visibility
+        nav_home.setVisible(false);
+        nav_info.setVisible(false);
+        nav_personalData.setVisible(false);
+        nav_medicalRecords.setVisible(false);
+        nav_questionnaires.setVisible(false);
+    }
+
+    public void removeItemAdmin(){
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // get menu from navigationView
+        Menu menu = navigationView.getMenu();
+        // find MenuItem you want to change
+        nav_addUser = menu.findItem(R.id.nav_add_user);
+        nav_readRatings = menu.findItem(R.id.nav_read_ratings);
+        nav_addAcceptance = menu.findItem(R.id.nav_add_new_acceptance);
+        nav_addRetrieveNecessities = menu.findItem(R.id.nav_add_retrive_necessities);
+        //Set item visibility
+        nav_addUser.setVisible(false);
+        nav_readRatings.setVisible(false);
+        nav_addAcceptance.setVisible(false);
+        nav_addRetrieveNecessities.setVisible(false);
+    }
+
+    public void getUserRole(){
+        // Initialize FirebaseUser
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        mUserReference = FirebaseDatabase.getInstance().getReference("user").child(user.getUid());
+
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get User object and use the values to update the UI
+                User user = dataSnapshot.getValue(User.class);
+                int role = user.getRole();
+                if (role == 2) {    //role 2 = User
+                    removeItemAdmin();
+                    removeItemDoctor();
+                } else if (role == 3) { //role 3 = Doctor
+                    removeItemAdmin();
+                    removeItemUser();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting User failed, log a message
+                Log.w(TAG, "loadUser:onCancelled", databaseError.toException());
+                Toast.makeText(PathologyActivity.this, "Failed to load user.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        mUserReference.addListenerForSingleValueEvent(userListener);
     }
 }
