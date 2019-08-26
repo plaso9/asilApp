8920719718f244ece85a81de0899f01e20e877d9
plaso9 <piepoligiulio7@gmail.com>
2019-08-26 17:53:10 +0200

    Create drawer menu for user role 'doctor'

--- app/src/main/java/it/uniba/di/sms/asilapp/DoctorActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/DoctorActivity.java
@@ -1,14 +1,24 @@
 package it.uniba.di.sms.asilapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 
-public class DoctorActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DoctorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     //Variable declaration
     CardView card_view_kitOpening;
     CardView card_view_patientList;
@@ -16,13 +26,61 @@ public class DoctorActivity extends AppCompatActivity {
     CardView card_view_video;
     FloatingActionButton chatButton;
     GridLayout gridLayout;
+    private DrawerLayout drawer;
 
+    private MenuItem nav_addUser;
+    private MenuItem nav_homeAdmin;
+    private MenuItem nav_readRatings;
+    private MenuItem nav_addAcceptance;
+    private MenuItem nav_addRetrieveNecessities;
+    private MenuItem nav_home;
+    private MenuItem nav_info;
+    private MenuItem nav_personalData;
+    private MenuItem nav_medicalRecords;
+    private MenuItem nav_questionnaires;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Set the activity content from a layout resource.
         setContentView(R.layout.activity_doctor);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // get menu from navigationView
+        Menu menu = navigationView.getMenu();
+
+        // find MenuItem you want to change
+        nav_home = menu.findItem(R.id.nav_home);
+        nav_info = menu.findItem(R.id.nav_info);
+        nav_addUser = menu.findItem(R.id.nav_add_user);
+        nav_homeAdmin = menu.findItem(R.id.nav_homeAdmin);
+        nav_readRatings = menu.findItem(R.id.nav_read_ratings);
+        nav_personalData = menu.findItem(R.id.nav_personalData);
+        nav_addAcceptance = menu.findItem(R.id.nav_add_new_acceptance);
+        nav_medicalRecords = menu.findItem(R.id.nav_medicalRecords);
+        nav_questionnaires = menu.findItem(R.id.nav_questionnaires);
+        nav_addRetrieveNecessities = menu.findItem(R.id.nav_add_retrive_necessities);
+        //Set item visibility
+        nav_home.setVisible(false);
+        nav_info.setVisible(false);
+        nav_addUser.setVisible(false);
+        nav_homeAdmin.setVisible(false);
+        nav_readRatings.setVisible(false);
+        nav_personalData.setVisible(false);
+        nav_addAcceptance.setVisible(false);
+        nav_medicalRecords.setVisible(false);
+        nav_questionnaires.setVisible(false);
+        nav_addRetrieveNecessities.setVisible(false);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         //defined gridlayout variable
         gridLayout=findViewById(R.id.gridDoctorLayout);
@@ -87,4 +145,49 @@ public class DoctorActivity extends AppCompatActivity {
         }
     };
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_homeDoctor:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_homeDoctorIntent = new Intent (DoctorActivity.this, DoctorActivity.class);
+                startActivity(nav_homeDoctorIntent);
+                break;
+            case R.id.nav_search_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_searchPatientIntent = new Intent (DoctorActivity.this, SearchPatientActivity.class);
+                startActivity(nav_searchPatientIntent);
+                break;
+            case R.id.nav_kit_opening:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_kitOpeningIntent = new Intent (DoctorActivity.this, KitOpeningActivity.class);
+                startActivity(nav_kitOpeningIntent);
+                break;
+            case R.id.nav_visited_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_visitedPatientIntent = new Intent (DoctorActivity.this, PatientListActivity.class);
+                startActivity(nav_visitedPatientIntent);
+                break;
+            case R.id.nav_video:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_videoIntent = new Intent (DoctorActivity.this, VideoActivity.class);
+                startActivity(nav_videoIntent);
+                break;
+            case R.id.nav_logout:
+                drawer.closeDrawer(GravityCompat.START);
+                //Sign out function
+                FirebaseAuth.getInstance().signOut();
+                //Create new Intent
+                Intent nav_logoutIntent = new Intent(DoctorActivity.this, MainActivity.class);
+                startActivity(nav_logoutIntent);
+                finish();
+                break;
+        }
+        return true;
+    }
 }
