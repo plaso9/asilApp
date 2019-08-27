4707cb082db969aa1c90087d3dd39b50378636ea
plaso9 <piepoligiulio7@gmail.com>
2019-08-27 08:05:35 +0200

    Drawer in kit opening

--- app/src/main/java/it/uniba/di/sms/asilapp/KitOpeningActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/KitOpeningActivity.java
@@ -11,7 +11,15 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,11 +27,14 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class KitOpeningActivity extends Activity {
+public class KitOpeningActivity extends Activity implements NavigationView.OnNavigationItemSelectedListener{
 
     ListView listViewPaired;
     ListView listViewDetected;
@@ -40,11 +51,60 @@ public class KitOpeningActivity extends Activity {
     ArrayList<BluetoothDevice> arrayListBluetoothDevices = null;
     ListItemClicked listItemClicked;
 
+    private DrawerLayout drawer;
+    private MenuItem nav_addUser;
+    private MenuItem nav_homeAdmin;
+    private MenuItem nav_readRatings;
+    private MenuItem nav_addAcceptance;
+    private MenuItem nav_addRetrieveNecessities;
+    private MenuItem nav_home;
+    private MenuItem nav_info;
+    private MenuItem nav_personalData;
+    private MenuItem nav_medicalRecords;
+    private MenuItem nav_questionnaires;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kit_opening);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // get menu from navigationView
+        Menu menu = navigationView.getMenu();
+
+        // find MenuItem you want to change
+        nav_home = menu.findItem(R.id.nav_home);
+        nav_info = menu.findItem(R.id.nav_info);
+        nav_addUser = menu.findItem(R.id.nav_add_user);
+        nav_homeAdmin = menu.findItem(R.id.nav_homeAdmin);
+        nav_readRatings = menu.findItem(R.id.nav_read_ratings);
+        nav_personalData = menu.findItem(R.id.nav_personalData);
+        nav_addAcceptance = menu.findItem(R.id.nav_add_new_acceptance);
+        nav_medicalRecords = menu.findItem(R.id.nav_medicalRecords);
+        nav_questionnaires = menu.findItem(R.id.nav_questionnaires);
+        nav_addRetrieveNecessities = menu.findItem(R.id.nav_add_retrive_necessities);
+        //Set item visibility
+        nav_home.setVisible(false);
+        nav_info.setVisible(false);
+        nav_addUser.setVisible(false);
+        nav_homeAdmin.setVisible(false);
+        nav_readRatings.setVisible(false);
+        nav_personalData.setVisible(false);
+        nav_addAcceptance.setVisible(false);
+        nav_medicalRecords.setVisible(false);
+        nav_questionnaires.setVisible(false);
+        nav_addRetrieveNecessities.setVisible(false);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         listViewDetected = (ListView) findViewById(R.id.listViewDetected);
         listViewPaired = (ListView) findViewById(R.id.listViewPaired);
         buttonSearch = (Button) findViewById(R.id.buttonSearch);
@@ -96,6 +156,53 @@ public class KitOpeningActivity extends Activity {
             adapter.notifyDataSetChanged();
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_homeDoctor:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_homeDoctorIntent = new Intent (KitOpeningActivity.this, DoctorActivity.class);
+                startActivity(nav_homeDoctorIntent);
+                break;
+            case R.id.nav_search_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_searchPatientIntent = new Intent (KitOpeningActivity.this, SearchPatientActivity.class);
+                startActivity(nav_searchPatientIntent);
+                break;
+            case R.id.nav_kit_opening:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_kitOpeningIntent = new Intent (KitOpeningActivity.this, KitOpeningActivity.class);
+                startActivity(nav_kitOpeningIntent);
+                break;
+            case R.id.nav_visited_patient:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_visitedPatientIntent = new Intent (KitOpeningActivity.this, PatientListActivity.class);
+                startActivity(nav_visitedPatientIntent);
+                break;
+            case R.id.nav_video:
+                drawer.closeDrawer(GravityCompat.START);
+                //Create new Intent
+                Intent nav_videoIntent = new Intent (KitOpeningActivity.this, VideoActivity.class);
+                startActivity(nav_videoIntent);
+                break;
+            case R.id.nav_logout:
+                drawer.closeDrawer(GravityCompat.START);
+                //Sign out function
+                FirebaseAuth.getInstance().signOut();
+                //Create new Intent
+                Intent nav_logoutIntent = new Intent(KitOpeningActivity.this, MainActivity.class);
+                startActivity(nav_logoutIntent);
+                finish();
+                break;
+        }
+        return true;
+    }
+
     class ListItemClicked implements AdapterView.OnItemClickListener
     {
         @Override
