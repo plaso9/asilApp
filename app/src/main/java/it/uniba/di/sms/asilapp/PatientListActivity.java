92fd2ab79ca9f18a411c0b39c9c8652ad908631b
dpalmisano26 <palmisano95lp@gmail.com>
2019-09-02 12:37:07 +0200

    Bug fix on marginEnd

--- app/src/main/java/it/uniba/di/sms/asilapp/PatientListActivity.java
+++ app/src/main/java/it/uniba/di/sms/asilapp/PatientListActivity.java
@@ -1,6 +1,5 @@
 package it.uniba.di.sms.asilapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
